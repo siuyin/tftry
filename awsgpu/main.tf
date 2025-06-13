43a080396ba93b715c764c5c0fb5a4bc286d9d35
Loh Siu Yin <siuyin@beyondbroadcast.com>
2025-06-13 10:42:06 +0800

    feat: use ubuntu nvidia image requiring 65GB disk

--- awsgpu/main.tf
+++ awsgpu/main.tf
@@ -12,19 +12,22 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
-# ollama fails to find cuda drivers on debian
-# data "aws_ami" "debian" {
-#   most_recent = true
-#   name_regex ="debian-12"
-#   filter {
-#     name = "architecture"
-#     values = ["arm64"]
-#   }
-# }
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["Deep Learning ARM64 Base OSS Nvidia Driver GPU AMI (Ubuntu 22.04)*"]
+  }
+  filter {
+    name = "architecture"
+    values = ["arm64"]
+  }
+}
 
 resource "aws_instance" "siuyin" {
   #ami           = "ami-00d2a3841e190f297" # ubuntu 22.04, user: ubuntu
-  ami           = "ami-0608188965cc1a3c8" # amazon linux 2023, user: ec2-user
+  #ami           = "ami-0608188965cc1a3c8" # amazon linux 2023, user: ec2-user
+  ami           = data.aws_ami.ubuntu.id
   instance_type = "g5g.xlarge"
   key_name = "id_rsa"
 
